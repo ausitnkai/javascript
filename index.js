0535679fee09f842017f5c3c0aa2de87275af5df
AUSTIN KAI <hhh123971@gmail.com>
2023-11-18 15:00:11 +0800

    變數概念

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 console.log("I like pizza!!!!"); //用於網頁的主控制台來紀錄訊息
 console.log("我正在學習");
 
-window.alert("我真的好想睡覺~~~~~");// 網頁的彈出式視窗
+window.alert("一個基本的網頁操作~~~~~");// 網頁的彈出式視窗
+
+/*
+ A variable 變數的相關操作
+ 
+ 兩步驟:
+ 1. declaration 宣告型別 
+ var : 宣告一個可隨意更改其內容的變數
+ let : 宣告一個可隨意更改其內容的 "區塊區域" 變數
+ const : 宣告一個只可讀取的不可變常數
+
+ 2. assigment 賦值 (= 賦予相對應的值)
+*/
+
+let age = 20; //整數型別
+let firstName = "kai"; //字串型別
+let student = true; // 布林型別
+
+
+console.log("Hello", firstName);
+console.log("You are", age, "years old.");
+console.log(student);
+
+document.getElementById("p1").innerHTML = "Hello " + firstName;
+document.getElementById("p2").innerHTML = "You are " + age + " years old.";
+document.getElementById("p3").innerHTML = "你還是學生嗎? " + student;
