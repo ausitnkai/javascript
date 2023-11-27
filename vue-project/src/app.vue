36f2102058ed51bf075b0043a6f85d7ad672b8ce
AUSTIN KAI <hhh123971@gmail.com>
2023-11-27 13:04:07 +0800

    vue相關

--- vue-project/src/app.vue
+++ vue-project/src/app.vue
@@ -1,7 +1,8 @@
 <!--javascript-->
 <script setup> 
+    import {ref} from "vue";
     let message = "Hello " + window.prompt("請輸入姓名","");
-    let checked = true;
+    let checked = false;
     let level=0;
     let topic=["HTML","CSS","JavaScript","Vue.js"];
     let objTopic={
@@ -11,6 +12,26 @@
         vue:"https://zh.wikipedia.org/wiki/Vue.js"
 
     };
+    // 事件處理
+    let handler=function () {
+       console.log("Click");
+    };
+    let mouseOverHandler=function () {
+        console.log("Mouse Over");
+    };
+
+    // 響應式狀態
+    let state=ref({
+        text:"網站標題",
+        className:"text"
+    });
+    let hit=function() {
+        /*text="新的網站內容"，這樣做並不會做同步更改*/ 
+        state.value.text="新的網站內容";
+        state.value.className="newText"
+    };
+    
+
     console.log("我的第一個組件")
 </script>
 <!--HTML-->
@@ -35,9 +56,25 @@
             <li v-for="(name,index) in topic">{{ index }} - {{ name }}</li>
         </ul>
     </div>
+    <hr class="hr-twill-colorful">
 
     <!-- vue 的 for 迴圈應用於物件-->
-    <div v-for="(value, key) in objTopic">推薦 {{ key }} 的相關介紹 {{value}}</div>
+    <!--使用v-bind或簡寫的冒號（:）來動態綁定 href 屬性-->
+    <div v-for="(value, key) in objTopic">推薦 {{ key }} 的
+        <a :href="value" target="_blank">相關介紹</a>
+    </div>
+    <hr class="hr-twill-colorful">
+
+    <!--基本事件處理-->
+    <div @click="handler" @mouseover.once="mouseOverHandler">點我</div>
+    <!--將預設事件處理取消-->
+    <a @click.prevent="handler" href="https://github.com/ausitnkai">我的網站</a>
+    <hr class="hr-twill-colorful">
+
+    <!--響應式狀態-->
+    <div :class="state.className">{{ state.text }}</div>
+    <button @click="hit">點我</button>
+
 </template>
 <!--CSS-->
 <style scoped> 
@@ -62,8 +99,23 @@
     /* 動畫效果的設定 */
     @keyframes animate {
         0% {background-color: #f7d3d3; left: 0px; top: 0px;}
-        50% {background-color: rgb(253, 125, 176); left:500px; top: 30px;}
+        50% {background-color: rgb(253, 125, 176); left:300px; top: 30px;}
         100% {background-color: rgb(255, 0, 0); left: 1000px; top: 50px;border-radius: 50%;}
     }
-
+    /*彩虹分隔線樣式*/
+    .hr-twill-colorful{
+        border: 0;
+        padding: 3px;
+        background: linear-gradient(135deg, red, orange, green ,blue, purple);
+        --mask-image: repeating-linear-gradient(135deg, #000 0px, #000 1px, transparent 1px, transparent 6px);
+        -webkit-mask-image: var(--mask-image);
+        mask-image: var(--mask-image);
+    }
+    .text{
+        font-weight: bold;
+    }
+    .newText{
+        font-weight: bold;
+        color: red;
+    }
 </style>
